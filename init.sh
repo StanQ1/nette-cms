350822adbfe5a6757e234b4539c737d4117ec306
Max Stanishevsky <xa1per11@gmail.com>
2025-01-20 01:38:57 +0100

    Created init.sh script and has filled default values in .env and .neon files

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Copying .env file"
+cp .env.example .env
+
+echo "Please, fill your own data into the .env file before proceeding."
+read -r -p "Press Enter to confirm that you have filled the .env file..."
+
+echo "Starting docker containers"
+sudo docker-compose up -d --build
+
+echo "Creating and setting right permissions to writable folders"
+docker exec -it $(docker ps -q -f name=php-container) mkdir -p /application/temp /application/log
+docker exec -it $(docker ps -q -f name=php-container) chmod -R 775 /application/temp /application/log
+
+echo "Copying app configuration file"
+docker exec -it $(docker ps -q -f name=php-container) cp /application/config.example.neon /application/config/config.neon
+
+echo "Please, fill your own data into the config.neon file before proceeding."
+read -r -p "Press Enter to confirm that you have filled the config.neon file..."
+
+echo "Installing dependencies"
+docker exec -it $(docker ps -q -f name=php-container) composer update
+docker exec -it $(docker ps -q -f name=php-container) composer install
+
+echo "Running database migrations..."
+docker exec -it $(docker ps -q -f name=php-container) php vendor/bin/phinx migrate
+
+echo "Setup complete!"
