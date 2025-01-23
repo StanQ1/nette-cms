5882c3b7cd6f6c337dbc85ee6858a4a08758365e
Max Stanishevsky <xa1per11@gmail.com>
2025-01-23 01:31:27 +0100

    init.sh copying config file

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+echo "!IMPORTANT! DON'T FORGET ABOUT WRITING YOUR OWN DATA INTO CONFIGS LIKE A .ENV IN PROJECT CORE AND CONFIG.NEON IN CONFIG FOLDER"
+
 echo "Installing dependencies"
 sudo docker exec -it php-container composer update
 sudo docker exec -it php-container composer install
+
+echo "Copying config.neon"
+sudo docker exec -it php-container cp config.example.neon config/config.neon
 
 echo "Running database migrations..."
 sudo docker exec -it php-container php /var/www/html/vendor/bin/phinx migrate
