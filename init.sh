9da9750cac498fe0588a9c573f410d49038f12ae
Max Stanishevsky <xa1per11@gmail.com>
2025-01-23 01:20:45 +0100

    fixed sql [1045]

--- init.sh
+++ init.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
-
-echo "Copying .env file"
-cp .env.example .env
-
-echo "Please, fill your own data into the .env file before proceeding."
-read -r -p "Press Enter to confirm that you have filled the .env file..."
-
-echo "Starting docker containers"
-sudo docker-compose up -d --build
-
-echo "Creating and setting right permissions to writable folders"
-sudo docker exec -it php-container mkdir -p /application/temp /application/log
-sudo docker exec -it php-container chmod -R 775 /application/temp /application/log
-
-echo "Copying app configuration file"
-sudo docker exec -it php-container cp /application/config.example.neon /application/config/config.neon
-
-echo "Please, fill your own data into the config.neon file before proceeding."
-read -r -p "Press Enter to confirm that you have filled the config.neon file..."
-
 echo "Installing dependencies"
 sudo docker exec -it php-container composer update
 sudo docker exec -it php-container composer install
 
 echo "Running database migrations..."
-sudo docker exec -it php-container php vendor/bin/phinx migrate
+sudo docker exec -it php-container php /var/www/html/vendor/bin/phinx migrate
 
 echo "Setup complete!"
