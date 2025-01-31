9f2691f0c9c0bf566a8e4d49ae8b05ed5a9b0744
Max Stanishevsky <xa1per11@gmail.com>
2025-01-31 23:20:26 +0100

    Auto login after register

--- application/app/UI/AuthModule/Register/RegisterPresenter.php
+++ application/app/UI/AuthModule/Register/RegisterPresenter.php
@@ -4,6 +4,7 @@ namespace App\UI\AuthModule\Register;
 
 use App\Core\BasePresenter;
 use App\Forms\Auth\RegistrationForm;
+use App\Model\UserModel;
 use App\Services\ConfigurationService;
 use App\Services\UserService;
 use Nette\Application\UI\Form;
@@ -13,6 +14,7 @@ class RegisterPresenter extends BasePresenter
     public function __construct(
         private readonly RegistrationForm       $form,
         private readonly UserService            $userService,
+        private readonly UserModel $userModel,
         protected ConfigurationService $configurationService,
     ){
         parent::__construct($this->configurationService);
@@ -29,13 +31,22 @@ class RegisterPresenter extends BasePresenter
     public function processForm(Form $form, $values): void
     {
         try {
+
             $this->userService->createUser(
                 $values->username,
                 $values->password,
             );
+
+            $user = $this->userModel->findByUsername($values->username)->fetch();
+
+            $sessionSection = $this->getSession('user');
+            $sessionSection->userId = $user->id;
+            $sessionSection->username = $user->username;
+            $sessionSection->permissionLevel = $user->permissionLevel;
         } catch (\Exception $e) {
-            $form->addError($e->getMessage());
+            $this->flashMessage($e->getMessage(), 'error');
         }
+        $this->redirect(':Front:Home:default');
     }
     public function renderDefault(): void
     {
