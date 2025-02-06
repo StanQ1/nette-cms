a27f1b78a3761745bdb0b708dd9a0e4259a0ae9f
Max Stanishevsky <xa1per11@gmail.com>
2025-02-06 15:26:50 +0100

    Form process logic to creating adn editing articles

--- application/app/UI/AdminModule/Article/ArticlePresenter.php
+++ application/app/UI/AdminModule/Article/ArticlePresenter.php
@@ -2,13 +2,17 @@
 
 namespace App\UI\AdminModule\Article;
 
+use App\Forms\Admin\Article\ArticleCreateForm;
+use App\Forms\Admin\Article\ArticleEditForm;
 use App\Services\ArticleService;
+use Nette\Application\UI\Form;
 use Nette\Application\UI\Presenter;
 
 final class ArticlePresenter extends Presenter
 {
-    public function __construct(private readonly ArticleService $articleService)
-    {
+    public function __construct(
+        private readonly ArticleService $articleService
+    ){
         parent::__construct();
     }
     public function renderDefault(): void
@@ -16,16 +20,78 @@ final class ArticlePresenter extends Presenter
         $this->template->articles = $this->articleService->getAllArticles();
     }
 
+    protected function createComponentArticleCreateForm(): Form
+    {
+        $categories = $this->articleService->getAllCategories();
+        $form = (new ArticleCreateForm())->create($categories);
+        $form->onSuccess[] = [$this, 'processArticleCreateForm'];
+
+        return $form;
+    }
+
+    public function processArticleCreateForm(Form $form, $values): void
+    {
+        $result = $this->articleService->createArticle(
+            title: $values->title,
+            categoryId: $values->category,
+            content: $values->content,
+        );
+
+        if ($result) {
+            $this->flashMessage("Article '$values->title' was successful created!");
+            $this->redirect('default');
+        } else {
+            $this->flashMessage('Article was not created!');
+        }
+    }
+
     public function renderCreate(): void
     {
-        // TODO: Write form processing logic
+    }
+
+    protected function createComponentArticleEditForm(): Form
+    {
+        $categories = $this->articleService->getAllCategories();
+
+        $form = (new ArticleEditForm())->create(
+            $categories,
+        );
+        $form->onSuccess[] = [$this, 'processArticleEditForm'];
+
+        return $form;
+    }
+
+    public function processArticleEditForm(Form $form, $values): void
+    {
+        $result = $this->articleService->editArticle(
+            articleId: (int) $values->id,
+            title: $values->title,
+            categoryId: (int) $values->category,
+            content: $values->content,
+        );
+
+        if ($result) {
+            $this->flashMessage("Article '$values->title' was successful edited!");
+            $this->redirect('default');
+        } else {
+            $this->flashMessage('Article wasn\'t edited!');
+        }
     }
 
 
     public function renderEdit(int $id): void
     {
-        // TODO: Write form processing logic
-        $this->template->article = $this->articleService->findArticleById($id);
+        $currentArticle = $this->articleService
+            ->findArticleById($id);
+        $this->getComponent('articleEditForm')
+            ->setDefaults([
+                'id' => $id,
+                'title' => $currentArticle->title,
+                'category' => $currentArticle->category_id,
+                'content' => $currentArticle->content,
+            ]);
+
+        $this->template->article = $currentArticle;
     }
 
     public function actionDelete(int $id): void
