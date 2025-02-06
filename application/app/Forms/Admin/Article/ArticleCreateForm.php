5a6860655bcac1fbd37ca78b57675748c53ed463
Max Stanishevsky <xa1per11@gmail.com>
2025-02-06 15:26:08 +0100

    Created forms to create and edit some articles

---
+++ application/app/Forms/Admin/Article/ArticleCreateForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Forms\Admin\Article;
+
+use Nette\Application\UI\Form;
+use Nette\Database\Table\Selection;
+
+class ArticleCreateForm
+{
+    /**
+     * @param Selection $categories
+     * @return Form
+     */
+    public function create(Selection $categories): Form
+    {
+        $form = new Form();
+
+        $processedCategories = [];
+
+        foreach ($categories as $category) {
+            $processedCategories[] = [
+                $category->category_id => $category->category_name,
+            ];
+        }
+
+        $form
+            ->addText('title', 'Title: ')
+            ->setRequired();
+        $form
+            ->addTextArea('content', 'Content: ')
+            ->setRequired();
+        $form
+            ->addSelect('category', 'Category: ', $processedCategories)
+            ->setPrompt('Pick a category')
+            ->setRequired();
+        $form
+            ->addSubmit('send', 'Create');
+
+        return $form;
+    }
+}
---
+++ application/app/Forms/Admin/Article/ArticleEditForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Forms\Admin\Article;
+
+use Nette\Application\UI\Form;
+use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\Selection;
+
+class ArticleEditForm
+{
+    /**
+     * @param Selection $categories
+     * @return Form
+     */
+    public function create(Selection $categories): Form
+    {
+        $form = new Form();
+
+        $processedCategories = [];
+
+        foreach ($categories as $category) {
+            $processedCategories[] = [
+                $category->category_id => $category->category_name,
+            ];
+        }
+
+        $form
+            ->addHidden('id');
+        $form
+            ->addText('title', 'Title: ');
+        $form
+            ->addTextArea('content', 'Content: ');
+        $form
+            ->addSelect('category', 'Category: ', $processedCategories)
+            ->setPrompt('Pick a category');
+        $form
+            ->addSubmit('send', 'Edit');
+
+        return $form;
+    }
+}
