86d472cc1270eeedbabf9f161b28558cfea88f37
Max Stanishevsky <xa1per11@gmail.com>
2025-02-06 03:28:06 +0100

    Created another views to ArticlePresenter.php

--- application/app/UI/AdminModule/Article/ArticlePresenter.php
+++ application/app/UI/AdminModule/Article/ArticlePresenter.php
@@ -2,17 +2,38 @@
 
 namespace App\UI\AdminModule\Article;
 
-use App\Model\ArticleModel;
+use App\Services\ArticleService;
 use Nette\Application\UI\Presenter;
 
 final class ArticlePresenter extends Presenter
 {
-    public function __construct(private readonly ArticleModel $articleModel)
+    public function __construct(private readonly ArticleService $articleService)
     {
         parent::__construct();
     }
     public function renderDefault(): void
     {
-        $this->template->articles = $this->articleModel->findAll();
+        $this->template->articles = $this->articleService->getAllArticles();
+    }
+
+    public function renderCreate(): void
+    {
+        // TODO: Write form processing logic
+    }
+
+
+    public function renderEdit(int $id): void
+    {
+        // TODO: Write form processing logic
+        $this->template->article = $this->articleService->findArticleById($id);
+    }
+
+    public function actionDelete(int $id): void
+    {
+        $result = $this->articleService->deleteArticle($id);
+        $response = $result ? 'You have successfully deleted article!'
+            : 'Article not found!';
+        $this->flashMessage($response);
+        $this->redirectPermanent(':Admin:Article:default');
     }
 }
