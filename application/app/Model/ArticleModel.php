b613e02a8e32d3998e3a33f74acdea01178119d7
Max Stanishevsky <xa1per11@gmail.com>
2025-02-06 01:09:15 +0100

    Edited returns after testing

--- application/app/Model/ArticleModel.php
+++ application/app/Model/ArticleModel.php
@@ -9,11 +9,6 @@ class ArticleModel extends BaseModel
     protected string $table = 'articles';
     protected string $category_table = 'article_categories';
 
-    public function findAll(): \Nette\Database\Table\Selection
-    {
-        return $this->database->table($this->table);
-    }
-
     public function getTableName(): string
     {
         return $this->table;
@@ -24,14 +19,19 @@ class ArticleModel extends BaseModel
         return $this->category_table;
     }
 
+    public function findAll(): \Nette\Database\Table\Selection
+    {
+        return $this->database->table($this->table);
+    }
+
     public function findAllByCategoryId(int $category_id): \Nette\Database\Table\Selection
     {
-        return $this->database->table($this->table)->where('category_id', $category_id)->fetchAll();
+        return $this->database->table($this->table)->where('category_id', $category_id);
     }
 
     public function findById(int $id): \Nette\Database\Table\ActiveRow
     {
-        return $this->database->table($this->table)->where('id', $id)->first();
+        return $this->database->table($this->table)->get($id);
     }
 
     public function insert(array $data): bool
@@ -44,8 +44,8 @@ class ArticleModel extends BaseModel
         return $this->database->table($this->table)->where('id', $id)->update($data);
     }
 
-    public function delete(int $id): bool
+    public function delete(int $id): int
     {
-        return $this->database->table($this->table)->where('id', $id)->delete();
+        return $this->database->table($this->table)->get($id)->delete();
     }
 }
--- application/app/Services/ArticleService.php
+++ application/app/Services/ArticleService.php
@@ -7,7 +7,6 @@ use App\Model\ArticleModel;
 use Nette\Database\Table\ActiveRow;
 use Nette\Database\Table\Selection;
 
-// TODO: Make a tests about all functions
 class ArticleService extends BaseModel
 {
     public function __construct(
@@ -24,18 +23,26 @@ class ArticleService extends BaseModel
         ]);
     }
 
+    public function findArticleById(int $articleId): ActiveRow
+    {
+        return $this->articleModel->findById($articleId);
+    }
+
     public function editArticle(int $id, string $title, int $category_id, string $content): bool
     {
-        return $this->articleModel->findById($id)->update([
-            'title' => $title,
-            'category_id' => $category_id,
-            'content' => $content,
-        ]);
+        return $this->articleModel->update(
+            id: $id,
+            data: [
+                'title' => $title,
+                'category_id' => $category_id,
+                'content' => $content,
+            ],
+        );
     }
 
     public function deleteArticle(int $id): bool
     {
-        return $this->articleModel->findById($id)->delete();
+        return $this->articleModel->delete($id) > 0;
     }
 
     public function getAllArticles(): Selection
@@ -46,10 +53,5 @@ class ArticleService extends BaseModel
     public function getArticlesByCategoryId(int $categoryId): Selection
     {
         return $this->articleModel->findAllByCategoryId($categoryId);
-    }
-
-    public function getArticleById(int $articleId): ActiveRow
-    {
-        return $this->articleModel->findById($articleId);
     }
 }
