bb1f46aecf49f7cd3d93ae3de01a53c3f140b692
Max Stanishevsky <xa1per11@gmail.com>
2025-02-08 06:04:43 +0100

    Provided logs displaying to admin dashboard

--- application/app/UI/AdminModule/Dashboard/DashboardPresenter.php
+++ application/app/UI/AdminModule/Dashboard/DashboardPresenter.php
@@ -3,11 +3,16 @@
 namespace App\UI\AdminModule\Dashboard;
 
 use App\Core\BasePresenter;
+use App\Services\ActionLogService;
 
 final class DashboardPresenter extends BasePresenter
 {
+    public function __construct(
+        private readonly ActionLogService $actionLogService,
+    ){
+    }
     public function renderDefault(): void
     {
-        $this->template->username = $this->getSession('user')->username;
+        $this->template->recentActivities = $this->actionLogService->getPageOfLatestActionLogs(10);
     }
 }
