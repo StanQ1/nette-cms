0de08ed4d0f07049ebad8a3bb160974c3261545a
Max Stanishevsky <xa1per11@gmail.com>
2025-02-08 06:07:21 +0100

    Provided logs displaying to admin dashboard

--- application/app/UI/AuthModule/Logout/LogoutPresenter.php
+++ application/app/UI/AuthModule/Logout/LogoutPresenter.php
@@ -3,12 +3,25 @@
 namespace App\UI\AuthModule\Logout;
 
 use App\Core\BasePresenter;
+use App\Services\ActionLogService;
 
 class LogoutPresenter extends BasePresenter
 {
+    public function __construct(
+        private readonly ActionLogService  $actionLogService,
+    ){
+    }
     public function actionDefault()
     {
-        $this->session->getSection('user')->remove();
+        $user = $this->session->getSection('user');
+        if ($user->permissionLevel >= 2)
+        {
+            $this->actionLogService->createActionLog(
+                executorId: $user->userId,
+                action: "User $user->username logged out from administration module",
+            );
+        }
+        $user->remove();
         $this->flashMessage('You have been logged out!');
         $this->redirect('Login:default');
     }
--- application/app/UI/SetupModule/Setup/SetupPresenter.php
+++ application/app/UI/SetupModule/Setup/SetupPresenter.php
@@ -3,19 +3,18 @@
 namespace App\UI\SetupModule\Setup;
 
 use App\Core\BasePresenter;
-use App\Model\UserModel;
+use App\Services\ActionLogService;
 use Nette\Application\UI\Form;
 use App\Services\UserService;
-use Nette\DI\Attributes\Inject;
 
 
 final class SetupPresenter extends BasePresenter
 {
-    #[Inject]
-    public UserService $userService;
-
-    #[Inject]
-    public UserModel $userModel;
+    public function __construct(
+        private readonly UserService $userService,
+        private readonly ActionLogService $actionLogService,
+    ){
+    }
     protected function createComponentSettingsForm(): Form
     {
         // TODO: transit to Forms/Admin
@@ -25,8 +24,10 @@ final class SetupPresenter extends BasePresenter
         $form = new Form();
         $form->addText('project_name', 'Project name: ')->setDefaultValue($defaultProjectName)
             ->setRequired('Project name is important!');
+        $form->addHidden('current_project_name', $defaultProjectName);
         $form->addText('admin_username', 'Admin username: ')->setDefaultValue($defaultAdminUsername)
             ->setRequired('Your admin user should be exists!');
+        $form->addHidden('current_admin_username')->setDefaultValue($defaultAdminUsername);
         $form->addPassword('admin_password', 'Admin password: ')
             ->addRule($form::MinLength, 'Password must be at least %d characters', 8)
             ->setRequired('Password field not be empty!');
@@ -45,19 +46,58 @@ final class SetupPresenter extends BasePresenter
         // Create admin in DB while first setup
         if ($this->configurationService->getConfigurationValue('is_ready_to_use') == 0) {
             $this->userService->createUser($data->admin_username, $data->admin_password, 2);
+
+            $user = $this->userService->findUserByUsername($data->admin_username)->fetch();
+
+            $this->actionLogService->createActionLog(
+                executorId: $user->toArray()['id'],
+                action: "Created administrator account $data->admin_username",
+            );
+
+            $this->actionLogService->createActionLog(
+                executorId: $user->toArray()['id'],
+                action: "Project \"$data->project_name\" was configured by $data->admin_username",
+            );
+
+            $this->configurationService->setConfigurationValue('admin_username', $data->admin_username);
             $this->configurationService->setConfigurationValue('is_ready_to_use', 1);
         } else if ($this->configurationService->getConfigurationValue('is_ready_to_use') == 1) {
             $this->configurationService->setConfigurationValue('admin_username', $data->admin_username);
-            $this->configurationService->setConfigurationValue('admin_password', password_hash($data->admin_password, PASSWORD_BCRYPT));
-            $this->userService->editUser($this->userModel->findByUsername(
-                $this->getSession()->getSection('user')->get('username'))->fetch()->id,
+            $this->configurationService->setConfigurationValue(
+                'admin_password',
+                    password_hash($data->admin_password, PASSWORD_BCRYPT
+                ));
+
+            $user = $this->userService->findUserByUsername($data->current_admin_username)->fetch();
+
+            $this->userService->editUser(
+                $user->toArray()['id'],
                 $data->admin_username,
-                $data->admin_password
+                $data->admin_password,
+            );
+
+            if ($data->admin_username != $data->current_admin_username) {
+                $this->actionLogService->createActionLog(
+                    executorId: $this->getSession()->getSection('user')->userId,
+                    action: "Administrator $data->current_admin_username has changed username to \"$data->admin_username\"",
+                );
+            }
+
+            if ($data->current_project_name != $data->project_name) {
+                $this->actionLogService->createActionLog(
+                    executorId: $this->getSession()->getSection('user')->userId,
+                    action: "Administrator $data->admin_username has changed project name to \"$data->project_name\"",
+                );
+            }
+
+            $this->actionLogService->createActionLog(
+                executorId: $this->getSession()->getSection('user')->userId,
+                action: "Administrator $data->admin_username has updated project configuration",
             );
         }
 
         $this->flashMessage('Data is successful updated!');
-        $this->redirect(':Front:Home:default');
+        $this->redirect(':Admin:Dashboard:default');
     }
 
     public function renderDefault(): void
